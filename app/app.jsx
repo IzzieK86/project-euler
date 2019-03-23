1ec442d75e26b80fe53d3db0547c81d63f30d15b
Glitch (project-euler) <none>
2019-03-23 14:14:24 +0000

    🌟⚓️ Updated with Glitch

---
+++ app/app.jsx
@@ -0,0 +1,7 @@
+const React = require('react');
+const ReactDOM = require('react-dom');
+
+/* Import Components */
+const Problems = require('./components/Problems');
+
+ReactDOM.render(<Problems />, document.getElementById('main'));
